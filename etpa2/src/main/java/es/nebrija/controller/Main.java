f6955b1ef2bcc20a3edfe7e5a3b0ba106349067e
Theshy520-spec <q1314xfq@gmail.com>
2025-05-20 08:56:14 +0200

    .

--- etpa2/src/main/java/es/nebrija/controller/Main.java
+++ etpa2/src/main/java/es/nebrija/controller/Main.java
@@ -4,6 +4,6 @@ import es.nebrija.model.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Dobe come rollito primavera y Capi los moja");
     }
 }
