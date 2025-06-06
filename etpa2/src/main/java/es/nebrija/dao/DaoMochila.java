876c1baf486c38f22264a96cb1d905ea2686a7e7
Theshy520-spec <q1314xfq@gmail.com>
2025-06-06 23:19:04 +0200

    Mochila y Objetos

--- etpa2/src/main/java/es/nebrija/dao/DaoMochila.java
+++ etpa2/src/main/java/es/nebrija/dao/DaoMochila.java
@@ -1,31 +1,66 @@
 package es.nebrija.dao;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
+import es.nebrija.model.Mochila;
+
 public class DaoMochila {
-    private static final String URL = "";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private Connection conn;
 
-    private String nombre;
-    private String descripcion;
-    private int capacidad;
-    private int valor;
+    public DaoMochila() throws SQLException {
+        conn = DbConnection.getConnection();
+    }
 
+    public ArrayList<Mochila> cargarMochilas() throws SQLException {
+        String sql = "SELECT * FROM Mochila";
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
 
-    public void addObjeto(String nombre, String descripcion, int capacidad, int valor)throws Exception {
-        String sql = "INSERT INTO Objeto (nombre, descripcion, peso) VALUES (?, ?, ?)";
-            try (Connection conn= DriverManager.getConnection(URL, USER, PASSWORD);
-                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-                preparedStatement.setString(1, nombre);
-                preparedStatement.setString(2, descripcion);
-                preparedStatement.setInt(3, capacidad);
-                preparedStatement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
+            ArrayList<Mochila> mochilas = new ArrayList<>();
+            while (rs.next()) {
+                mochilas.add(new Mochila(
+                    rs.getInt("idMochila"),
+                    rs.getString("nombre"),
+                    rs.getString("descripcion"),
+                    rs.getInt("capacidad"),
+                    rs.getInt("valor")
+                ));
             }
+            return mochilas;
+        }
+    }
+   
+    public void guardarMochila(Mochila mochila) throws SQLException {
+        String sql = "INSERT INTO Mochila(nombre, descripcion, capacidad, valor) VALUES(?,?,?,?)";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, mochila.getNombre());
+            ps.setString(2, mochila.getDescripcion());
+            ps.setInt(3, mochila.getCapacidad());
+            ps.setInt(4, mochila.getValor());
+            ps.executeUpdate();
+        }
+    }
+    public void actualizarMochila(Mochila mochila) throws SQLException {
+        String sql = "UPDATE Mochila SET nombre=?, descripcion=?, capacidad=?, valor=? WHERE idMochila=?";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, mochila.getNombre());
+            ps.setString(2, mochila.getDescripcion());
+            ps.setInt(3, mochila.getCapacidad());
+            ps.setInt(4, mochila.getValor());
+            ps.setInt(5, mochila.getIdMochila());
+            ps.executeUpdate();
+        }
+    }
+    public void eliminarMochila(int idMochila) throws SQLException {
+        String sql = "DELETE FROM Mochila WHERE idMochila=?";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, idMochila);
+            ps.executeUpdate();
+        }
     }
 }
--- etpa2/src/main/java/es/nebrija/model/Objeto.java
+++ etpa2/src/main/java/es/nebrija/model/Objeto.java
@@ -1,5 +1,56 @@
 package es.nebrija.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import es.nebrija.dao.DaoObjeto;
+import es.nebrija.dao.DbConnection;
+
 public class Objeto {
-    
+    private int idObjeto;
+    private String nombre;
+    private String descripcion;
+    private int valor;
+    private int peso;
+
+    public Objeto(int idObjeto, String nombre, String descripcion, int valor, int peso) {
+        this.idObjeto = idObjeto;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.valor = valor;
+        this.peso = peso;
+    }
+    public void mostrarObjetos() throws SQLException {
+        ArrayList<Objeto> objetos = new ArrayList<Objeto>();
+        DaoObjeto cargarObjetos = new DaoObjeto();
+        objetos = cargarObjetos.cargarObjetos();
+        for (Objeto objeto : objetos) {
+            System.out.println("ID: " + objeto.idObjeto + ", Nombre: " + objeto.nombre + 
+                               ", Descripción: " + objeto.descripcion + 
+                               ", Valor: " + objeto.valor + 
+                               ", Capacidad Ocupada: " + objeto.peso);
+        }
+    }
+    public int getIdObjeto() {
+        return idObjeto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
 }
